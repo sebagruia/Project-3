7e399153030f00d39465749a24d6fd6004f6c498
Sebastian Gruia <34604352+sebagruia@users.noreply.github.com>
2019-02-18 22:28:30 +0100

    Collision function issue

--- js/app.js
+++ js/app.js
@@ -8,51 +8,34 @@ class Enemy {
         this.y = y;
         this.speed = speed;
         this.delay = delay;
-
-
-
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
-
-
-
     }
 
+    
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
-
-
     update(dt) {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
-let yPosition = [60, 140, 220];
-    let yNewPosition = [];
+        let yPosition = [60, 140, 220];
+        let yNewPosition = [];
         let curentIndex = yPosition.length;
         if (this.x >= 0) {
             this.x += this.speed * dt;
             if (this.x > 505) {
                 this.x = 0;
- 
-    
-    for (let i = 0; i < yPosition.length; i++) {
-        const randomNumber = Math.floor(Math.random() * curentIndex);
-        yNewPosition.push(yPosition[randomNumber]);
-        yPosition[i] = yNewPosition[i];
-        
-    }
-    yNewPosition = [];
-
-     this.y = yPosition[0];
-
-}
-
-
+                for (let i = 0; i < yPosition.length; i++) {
+                    const randomNumber = Math.floor(Math.random() * curentIndex);
+                    yNewPosition.push(yPosition[randomNumber]);
+                    yPosition[i] = yNewPosition[i];
+                }
+                yNewPosition = [];
+                this.y = yPosition[0];
             }
-        
-
-
+        }
     }
 
     // Draw the enemy on the screen, required method for game
@@ -70,48 +53,28 @@ class Player {
         this.x = x;
         this.y = y;
         this.sprite = 'images/char-horn-girl.png';
-
-
-
-
-
     }
 
+    
     update(dt) {
-        const checkImpact = () => {
-            for (enemy of allEnemies) {
-                if (enemy.this.x == player.this.x && enemy.this.y == player.this.y) {
+        
+            for (const enemy of allEnemies) {
+                if (enemy.x == this.x && enemy.y == this.y) {
                     this.x = 202;
                     this.y = 404;
-
                 }
-
+                console.log(enemy.x, this.x);
             }
+        
 
-        };
-
-        function checkFinalMove() {
-            //            const num =0;
-            //            const surviveCount = document.getElementsByClassName('survived_counter');
-
-
-            if (this.y <= 83) {
-                //                surviveCount.innerHTML = `<h4>${num++}</h4>`;
-
-            }
-
-
-        }
-
-    }
+     }
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
     }
 
-    counting() {
-
+    countingVictory() {
         const surviveCount = document.querySelector('.survived_counter');
         console.log(surviveCount);
         if (this.y <= 83) {
@@ -120,7 +83,7 @@ class Player {
             surviveCount.innerHTML = `${number}`;
         }
 
-    };
+    }
 
 
     handleInput(direction) {
@@ -158,16 +121,7 @@ class Player {
                 }
                 break;
         }
-
-
-
-
-
-
-
-
     }
-
 }
 
 
@@ -176,7 +130,7 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const allEnemies = [];
-const speeds = [150,200,450];
+const speeds = [150, 200, 450];
 let number = 0;
 let randomSpeedGenerator = function (array) {
     const randomNumber = Math.floor((Math.random() * array.length) + 1);
@@ -189,9 +143,10 @@ const player = new Player();
 const enemy1 = new Enemy(0, 60, randomSpeedGenerator(speeds));
 const enemy2 = new Enemy(0, 140, randomSpeedGenerator(speeds));
 const enemy3 = new Enemy(202, 220, randomSpeedGenerator(speeds));
-const enemy4 = new Enemy(302, 220, randomSpeedGenerator(speeds));
-const enemy5 = new Enemy(302, 400, randomSpeedGenerator(speeds));
+const enemy4 = new Enemy(202, 220, randomSpeedGenerator(speeds));
+const enemy5 = new Enemy(101, 140, randomSpeedGenerator(speeds));
 allEnemies.push(enemy1, enemy2, enemy3, enemy4, enemy5);
+
 
 
 
@@ -207,6 +162,6 @@ document.addEventListener('keyup', function (e) {
         40: 'down'
     };
     player.handleInput(allowedKeys[e.keyCode]);
-    player.counting();
-    //    console.log(this.y);
+    player.countingVictory();
+    
 });
