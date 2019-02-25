e2e890830be75b6c9dcbbfad7ece1509310121fa
Sebastian Gruia <34604352+sebagruia@users.noreply.github.com>
2019-02-25 15:50:48 +0100

    Allmost done

--- README.md
+++ README.md
@@ -2,24 +2,29 @@
 
 ## Table of Contents
 
-- [Instructions](#instructions)
-- [Contributing](#contributing)
-
-## Instructions
-
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
-
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
-
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
-
-## Contributing
+- [Setting the game](#setting_the_game)
+- [How to play](#how_to_play)
 
 
-```
 
-```
+## setting_the_game
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+* The game starts by selecting you favorite character;
+* The selection is set after you click the "Select your hero button";
+
+
+
+## how_to_play
+
+* You can move your Hero by pressing the left, right, up and down keys;
+* When you reach the blue squares, which represent "water", you won the game, and your victory will be recorded by the "Survived" counter;
+* If you are hit by your enemy, you will die and will be repositioned at the starting position, your death will be recorded by the "Eaten Alive" counter;
+* If you want to start fresh, resetting the counters and choosing a different Hero, you must refresh your browser.
+
+
+
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 // Enemies our player must avoid
 
 class Enemy {
-    constructor(x, y, speed, delay) {
+    constructor(x, y, speed) {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
         this.x = x;
         this.y = y;
         this.speed = speed;
-        this.delay = delay;
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
@@ -20,13 +19,14 @@ class Enemy {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
-        let yPosition = [60, 140, 220];
+        let yPosition = [60, 140, 220]; // different y positions that can be used
         let yNewPosition = [];
         let curentIndex = yPosition.length;
         if (this.x >= 0) {
             this.x += this.speed * dt;
             if (this.x > 505) {
                 this.x = 0;
+                // generating random y position for the enemy
                 for (let i = 0; i < yPosition.length; i++) {
                     const randomNumber = Math.floor(Math.random() * curentIndex);
                     yNewPosition.push(yPosition[randomNumber]);
@@ -45,6 +45,7 @@ class Enemy {
 
 }
 
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -53,30 +54,32 @@ class Player {
         this.x = x;
         this.y = y;
         this.sprite = 'images/char-horn-girl.png';
+
     }
 
 
     update(dt) {
-const eatenAlive = document.querySelector('.eatenalive_counter');
-        
+        const eatenAlive = document.querySelector('.eatenalive_counter');
+        // collision algoritm and implementation of the "eatenAlive" counter
         for (const enemy of allEnemies) {
             if (this.x < (enemy.x + 50) && (this.x + 50) > enemy.x && this.y < (enemy.y + 40) && (this.y + 40) > enemy.y) {
                 this.y = 404;
                 this.x = 202;
                 eatenAliveNumbr++;
                 eatenAlive.innerHTML = `${eatenAliveNumbr}`;
-               }
-            
+            }
+
         }
-
-
     }
-
+ 
+    
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
     }
 
+    
+//This deals with counting the Hero wins *****************
     countingVictory() {
         const surviveCount = document.querySelector('.survived_counter');
         victoryNumber++;
@@ -85,7 +88,7 @@ const eatenAlive = document.querySelector('.eatenalive_counter');
         this.y = 404;
     }
 
-
+//This deals with the movement of the Hero *****************
     handleInput(direction) {
 
         switch (direction) {
@@ -121,6 +124,47 @@ const eatenAlive = document.querySelector('.eatenalive_counter');
                 break;
         }
     }
+    
+//This function selects different Heros *****************
+    selectHero() {
+        const heroArray = document.getElementsByClassName('image-hero');
+        const moveLeft = document.querySelector('.fa-caret-left');
+        const moveRight = document.querySelector('.fa-caret-right');
+        const selector = document.getElementById('selector');
+        const heroLibrarySelector = document.getElementById('hero-library');
+        const selectH4Text = document.getElementById('selector');
+        let i = heroArray.length - 1;
+
+        moveLeft.addEventListener('click', (ev1)=> {
+            if (!heroArray[i].classList.contains('hidden') && i >= 1) {
+                heroArray[i].classList.add('hidden');
+                heroArray[i - 1].classList.remove('hidden');
+                i--;
+            }
+
+        });
+
+        
+        moveRight.addEventListener('click',  (ev2) => {
+            if (!heroArray[i].classList.contains('hidden') && i <= 3) {
+                heroArray[i].classList.add('hidden');
+                heroArray[i + 1].classList.remove('hidden');
+                i++;
+            }
+
+        });
+
+        // This event should set a new Hero, but unfortunately is not working
+        selector.addEventListener('click', (ev) => {
+            heroLibrarySelector.classList.add('hidden');
+            selectH4Text.classList.add('hidden');
+//         this.sprite = heroArray[i].innerHTML; //I CAN'T SET THE "this.sprite" TO ANOTHER URL OTHER THAN THE ONE THAT IS SET BY CONSTRUCTOR METHOD
+                                                    //IF YOU CAN GIVE ME A HELPING HAND HERE I WOULD MUCH APPRECIATE 
+
+        });
+
+    }
+
 }
 
 
@@ -129,15 +173,13 @@ const eatenAlive = document.querySelector('.eatenalive_counter');
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const allEnemies = [];
-const speeds = [150, 200, 450];
+const speeds = [150, 200, 450]; // different values for speed, that enemies can use
 let victoryNumber = 0;
 let eatenAliveNumbr = 0;
-let randomSpeedGenerator = function (array) {
+let randomSpeedGenerator = (array) => { // a function that generates random speeds 
     const randomNumber = Math.floor((Math.random() * array.length) + 1);
     return array[randomNumber - 1];
 };
-
-
 
 const player = new Player();
 const enemy1 = new Enemy(0, 60, randomSpeedGenerator(speeds));
@@ -147,14 +189,10 @@ const enemy4 = new Enemy(0, 220, randomSpeedGenerator(speeds));
 allEnemies.push(enemy1, enemy2, enemy3, enemy4);
 
 
-
-
-
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function (e) {
+player.selectHero();
+document.addEventListener('keyup', (e) => {
     //    console.log(e);
     var allowedKeys = {
         37: 'left',
@@ -162,8 +200,7 @@ document.addEventListener('keyup', function (e) {
         39: 'right',
         40: 'down'
     };
-    player.handleInput(allowedKeys[e.keyCode]);
-    //    player.countingVictory();
 
+    player.handleInput(allowedKeys[e.keyCode]);
 
 });
